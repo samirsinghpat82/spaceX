877ac5aa060c58e91bbd58b1da1c117584a7cd83
Samir Sigh <samirsinghpat82@gmail.com>
2020-10-19 23:45:37 +0530

    Updates

---
+++ precache-manifest.20d1430cbcc4c1b0a296d66e1084f2d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ec17a698d339f069cdb601be8e4ae51",
+    "url": "/spaceX/index.html"
+  },
+  {
+    "revision": "9f21fbe534da585ffd00",
+    "url": "/spaceX/static/css/main.4fb12195.chunk.css"
+  },
+  {
+    "revision": "e4339c91129d5d0980a6",
+    "url": "/spaceX/static/js/2.1144decb.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/spaceX/static/js/2.1144decb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f21fbe534da585ffd00",
+    "url": "/spaceX/static/js/main.2e39171c.chunk.js"
+  },
+  {
+    "revision": "0177aedc8c117a0a217c",
+    "url": "/spaceX/static/js/runtime-main.fd86c7c5.js"
+  }
+]);
